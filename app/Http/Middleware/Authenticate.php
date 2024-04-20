83dd587092484e9d10f6b26abc0b09dd2451f078
Anass Benfillous <anasse.benfillous@gmail.com>
2024-04-20 16:56:24 +0100

    update auth middleware to check api authentication

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    public function handle($request, Closure $next, ...$guards)
     {
-        return $request->expectsJson() ? null : route('login');
+        if (!Auth::guard('api')->check()) {
+            return response()->json(['message' => 'Not authenticated']);
+        }
+
+        return $next($request);
     }
 }
