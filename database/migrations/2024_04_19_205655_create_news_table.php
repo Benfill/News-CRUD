9b0a2a9c9055c79932538b510d6c798a89da1192
Anass Benfillous <anasse.benfillous@gmail.com>
2024-04-20 04:18:26 +0100

    create migrations

---
+++ database/migrations/2024_04_19_205655_create_news_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('Titre');
+            $table->string('Contenu');
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->date('Date_debut');
+            $table->date('Date_expiration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
