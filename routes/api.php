3c8b384a7454ef6d40a88019204bc1c34e248021
Anass Benfillous <anasse.benfillous@gmail.com>
2024-04-20 04:16:24 +0100

    add login && logout routes with api middleware

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'Api', 'prefix' => 'v1'], function () {
+    Route::post('login', [AuthenticationController::class, 'store']);
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('logout', [AuthenticationController::class, 'destroy']);
+    });
 });
