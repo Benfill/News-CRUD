4b96ef7de63c12c7e9b836c89ffaeef67a3c5b64
Anass Benfillous <anasse.benfillous@gmail.com>
2024-04-20 16:56:51 +0100

    add slug col in news table

--- database/migrations/2024_04_19_205655_create_news_table.php
+++ database/migrations/2024_04_19_205655_create_news_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('news', function (Blueprint $table) {
             $table->id();
             $table->string('Titre');
+            $table->string('slug')->unique();
             $table->string('Contenu');
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->date('Date_debut');
