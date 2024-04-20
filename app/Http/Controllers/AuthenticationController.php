58f354d0a41564f9fc8f3678e1897442549d7448
Anass Benfillous <anasse.benfillous@gmail.com>
2024-04-20 04:16:52 +0100

    add login and logout functions

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthenticationController extends Controller
+{
+    public function store(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        $validator = Validator::make($credentials, [
+            'email' => 'required|email|exists:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        if (Auth::attempt($credentials)) {
+            // successfull authentication
+            $user = User::find(Auth::user()->id);
+
+            $user_token['token'] = $user->createToken('appToken')->accessToken;
+
+            return response()->json([
+                'success' => true,
+                'token' => $user_token,
+                'user' => $user,
+            ], 200);
+        } else {
+            // failure to authenticate
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to authenticate.',
+            ], 401);
+        }
+    }
+
+    public function destroy(Request $request)
+    {
+        if (Auth::user()) {
+            $request->user()->token()->revoke();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Logged out successfully',
+            ], 200);
+        } else {
+            // failure to authenticate
+            return response()->json([
+                'success' => false,
+                'message' => 'User already authenticated.',
+            ], 401);
+        }
+    }
+}
