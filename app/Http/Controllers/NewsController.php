4954732ec8edaa529db86c4f94934c00d22adea3
Anass Benfillous <anasse.benfillous@gmail.com>
2024-04-20 04:41:49 +0100

    create crud functions and implement index function to retrieve news sorted and not expired

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
 use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    //
+    function index()
+    {
+        $news = News::where('Date_expiration', '>', now())
+            ->orderBy('Date_debut', 'desc')
+            ->get();
+        if ($news->count() > 0) {
+            return response()->json(['news' => $news, 'success' => true])->status(200);
+        } else {
+            return response()->json(['success' => false])->status(400);
+        }
+
+    }
+
+    function store(Request $request)
+    {
+
+    }
+
+    function update($id)
+    {
+
+    }
+
+    function show($slug)
+    {
+
+    }
+
+    function destroy(Request $request)
+    {
+
+    }
 }
